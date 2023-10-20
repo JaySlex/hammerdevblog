0b6f7ac9dd824cfee33d2ebc0ec5634592cd688a
Iles Belkessam <iles.belkessam@gmail.com>
2023-10-20 00:06:20 -0400

    analytics

---
+++ analytics.js
@@ -0,0 +1,17 @@
+const page = 2;
+const url = "https://script.google.com/macros/s/AKfycbxkIWHe7BTsuyZ79RU56C_vJByZG0loOU4_wZK57eU4mWF4G3gydK0dLUNjlXj6RzQP/exec";
+const view = "?function=clicked&page="+page;
+const cl = "?function=button&page=";
+
+document.addEventListener("DOMContentLoaded", init);
+
+
+function init()
+{
+    fetch(url+view, { method: 'POST'})
+    .then(res => res.text())
+    .then(rep=>
+    {
+        console.log(rep);
+    });
+}
